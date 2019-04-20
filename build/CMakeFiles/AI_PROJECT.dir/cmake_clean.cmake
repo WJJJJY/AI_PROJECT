a24b31c251dd14a13401155ef447ba78360258b0
JinyuWang <wwang_jy@sina.com>
2019-04-20 17:57:31 +0800

    first commit

---
+++ build/CMakeFiles/AI_PROJECT.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AI_PROJECT.dir/srcs/renju.c.o"
+  "CMakeFiles/AI_PROJECT.dir/srcs/rjFunction.c.o"
+  "AI_PROJECT.pdb"
+  "AI_PROJECT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AI_PROJECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
